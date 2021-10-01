6425805988cff5cdd74df67950e7b59d252f394b
Muhamad Yamin <72939609+azarein@users.noreply.github.com>
2021-10-01 14:27:47 +0700

    Add files via upload

---
+++ SourceCode_2_7.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhamad Yamin
+ */
+public class SourceCode_2_7 {
+    public static void main(String[] args){
+        
+        int number = 10;
+        String jawab = number > 20 ? number + " Lebih besar dari 20" : number + "Lebih kecil dari 20";
+        
+        System.out.println(jawab);
+    }
+}
---
+++ Tugas2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhamad Yamin
+ */
+public class Tugas2 {
+    public static void main(String[] args) {
+        int uang = 50000;
+        int harga = 55000;
+        String hasil = uang > harga ? uang + "Uang Kurang 5000" : 
+                uang + " Uangnya tidak cukup";
+        System.out.println(hasil);
+    }
+    
+}
